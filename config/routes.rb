ec5ffd814d7b300f3eea4667d5821358ec93e29a
rolandfbk <rolandfbk@gmail.com>
2022-08-29 17:27:45 +0200

    Added Cancancan, Devise, rails rspec, create migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'foods#index'
+
+  resources :foods, only: [:index, :show, :new, :create, :destroy]
+  resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+    resources :recipe_foods, only: [:index, :show, :new, :create, :destroy]
+  end
+  resources :public_recipes, only: [:index]
+  resources :general_shopping_list, only: [:index]
 end
