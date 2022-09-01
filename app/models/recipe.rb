3c7e1ee2804c0f77be10d4c645015f1717159339
Masoudi Parrot <masoudi@localhost.localdomain>
2022-09-01 13:32:01 -0400

    add models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,10 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
 
+  # def public_recipes
+  #   Recipe.where(public: true).order(created_at: :desc)
+  # end
+
   validates :name, presence: true, length: { maximum: 250 }
   validates :preparation_time, presence: true, length: { maximum: 250 }
   validates :cooking_time, presence: true, length: { maximum: 250 }
