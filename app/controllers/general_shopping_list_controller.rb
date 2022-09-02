cee71a4b8737a9ae5e5f8f453b67d28090ea167d
Masoudi Parrot <masoudi@localhost.localdomain>
2022-09-02 09:42:36 -0400

    share params between action

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -3,12 +3,12 @@ class GeneralShoppingListController < ApplicationController
 
     def create
       @recipe = params[:recipe_id]
-      p @recipe
-      redirect_to general_shopping_list_index_path
+      redirect_to general_shopping_list_index_path(recipe: params[:recipe_id])
     end
 
     def index
       user_foods = current_user.foods
+      @recipe = Recipe.find(params[:recipe])
       recipe_foods = @recipe.recipe_foods.includes(:food)
       @food_items_to_buy = []
       recipe_foods.each do |rfood|
