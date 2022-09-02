eeefc268945e6d53043d2967c06758e35b849f76
rolandfbk <rolandfbk@gmail.com>
2022-09-02 10:39:20 +0200

    Shopping list to fix

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -3,11 +3,11 @@ class GeneralShoppingListController < ApplicationController
 
     def create
       @recipe = params[:recipe_id]
-      p @recipe
-      redirect_to general_shopping_list_index_path
+      redirect_to general_shopping_list_index_path(recipe: params[:recipe_id])
     end
 
     def index
+      @recipe = Recipe.find(params[:recipe])
       user_foods = current_user.foods
       recipe_foods = @recipe.recipe_foods.includes(:food)
       @food_items_to_buy = []
