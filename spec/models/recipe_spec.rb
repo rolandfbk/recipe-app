577099b1ba0c271b7b39115028de5d2a27dc2165
rolandfbk <rolandfbk@gmail.com>
2022-09-02 09:50:49 +0200

    model and controllers test done

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  user = User.create(name: 'Grabrielle')
+  subject do
+    Recipe.new(user_id: user.id, name: 'La soupe au choux', preparation_time: '2 hours', cooking_time: '1 hour',
+               description: 'La soupe au choux description')
+  end
+  before { subject.save }
+
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'preparation time should be present' do
+    subject.preparation_time = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'cooking time should be present' do
+    subject.cooking_time = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'description should be present' do
+    subject.description = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'description should be present' do
+    expect(subject.public).to be_falsey
+  end
 end
