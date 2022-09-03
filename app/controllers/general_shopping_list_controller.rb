27157a03f93186cd19fa928280824164ce95fc37
Masoudi Parrot <masoudi@localhost.localdomain>
2022-09-03 04:01:19 -0400

    retrive all the recipes foods

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -1,22 +1,14 @@
 class GeneralShoppingListController < ApplicationController
-  def create
-    @recipe = params[:recipe_id]
-    redirect_to general_shopping_list_index_path(recipe: params[:recipe_id])
-  end
-
+  
   def index
-    @recipe = Recipe.find(params[:recipe])
-    @recipe_foods = @recipe.recipe_foods.includes(:food)
-    @food_items_to_buy = []
-    @recipe_foods.each do |rfood|
-      item = {
-        name: rfood.food.name,
-        quantity: rfood.food.quantity < rfood.quantity ? rfood.quantity - rfood.food.quantity : rfood.quantity,
-        price: rfood.food.price,
-        measurement_unit: rfood.food.measurement_unit
-      }
-      @food_items_to_buy << item
+    user_foods = current_user.foods
+    all_foods = current_user.recipes.includes(recipe_foods: [:food]).map do |recipe|
+      recipe.recipe_foods.map { |recipe_food| recipe_food.food}
     end
+    recipes_foods = all_foods.flatten
+    @food_items_to_buy = user_foods.select{|food| food unless recipes_foods.include?(food) }
+    @total_foods = @food_items_to_buy.count
+    @total_price = @food_items_to_buy.map{|e| e.price}.sum
   end
 
   private
